b26d18a6ae954c3707c3b6d10456618f3710934b
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2017-01-24 09:07:48 +0100

    Moved shared functions into modules

---
+++ modules/HstwbInstaller-Dialog.psm1
@@ -0,0 +1,113 @@
+# print settings
+function PrintSettings()
+{
+    Write-Host "Settings"
+    Write-Host "  Settings File      : " -NoNewline -foregroundcolor "Gray"
+    Write-Host ("'" + $settingsFile + "'")
+    Write-Host "  Assigns File       : " -NoNewline -foregroundcolor "Gray"
+    Write-Host ("'" + $assignsFile + "'")
+    Write-Host "Image"
+    Write-Host "  HDF Image Path     : " -NoNewline -foregroundcolor "Gray"
+    Write-Host ("'" + $settings.Image.HdfImagePath + "'")
+    Write-Host "Workbench"
+    Write-Host "  Install Workbench  : " -NoNewline -foregroundcolor "Gray"
+    Write-Host ("'" + $settings.Workbench.InstallWorkbench + "'")
+    Write-Host "  Workbench Adf Path : " -NoNewline -foregroundcolor "Gray"
+    Write-Host ("'" + $settings.Workbench.WorkbenchAdfPath + "'")
+    Write-Host "  Workbench Adf Set  : " -NoNewline -foregroundcolor "Gray"
+    Write-Host ("'" + $settings.Workbench.WorkbenchAdfSet + "'")
+    Write-Host "Kickstart"
+    Write-Host "  Install Kickstart  : " -NoNewline -foregroundcolor "Gray"
+    Write-Host ("'" + $settings.Kickstart.InstallKickstart + "'")
+    Write-Host "  Kickstart Rom Path : " -NoNewline -foregroundcolor "Gray"
+    Write-Host ("'" + $settings.Kickstart.KickstartRomPath + "'")
+    Write-Host "  Kickstart Rom Set  : " -NoNewline -foregroundcolor "Gray"
+    Write-Host ("'" + $settings.Kickstart.KickstartRomSet + "'")
+    Write-Host "Packages"
+
+    $packageFileNames = @()
+    $packageFileNames += $settings.Packages.InstallPackages -split ',' | Where-Object { $_ }
+
+    if ($packageFileNames.Count -gt 0)
+    {
+        for ($i = 0; $i -lt $packageFileNames.Count;$i++)
+        {
+            if ($i -eq 0)
+            {
+                Write-Host "  Install Packages   : " -NoNewline -foregroundcolor "Gray"
+            }
+            else
+            {
+                Write-Host "                       " -NoNewline
+            }
+
+            $packageFileName = $packageFileNames[$i]
+            $package = $packages.Get_Item($packageFileName)
+
+            Write-Host ("'" + $package.Package.Name + " v" + $package.Package.Version + "'")
+        }
+    }
+    else
+    {
+        Write-Host "  Install Packages   : " -NoNewline -foregroundcolor "Gray"
+        Write-Host "None" -foregroundcolor "Yellow"
+    }
+
+    Write-Host "WinUAE"
+    Write-Host "  WinUAE Path        : " -NoNewline -foregroundcolor "Gray"
+    Write-Host ("'" + $settings.Winuae.WinuaePath + "'")
+    Write-Host "Installer"
+    Write-Host "  Mode               : " -NoNewline -foregroundcolor "Gray"
+    Write-Host ("'" + $settings.Installer.Mode + "'")
+}
+
+
+# enter path
+function EnterPath($prompt)
+{
+    do
+    {
+        $path = Read-Host $prompt
+
+        if ($path -ne '')
+        {
+            $path = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($path)
+        }
+        
+        if (!(test-path -path $path))
+        {
+            Write-Error "Path '$path' doesn't exist"
+        }
+    }
+    until ($path -eq '' -or (test-path -path $path))
+    return $path
+}
+
+
+# enter choice
+function EnterChoice($prompt, $options)
+{
+    $optionPadding = $options.Count.ToString().Length
+
+    for ($i = 0; $i -lt $options.Count; $i++)
+    {
+        Write-Host (("{0," + $optionPadding + "}: ") -f ($i + 1)) -NoNewline -foregroundcolor "Gray"
+        Write-Host $options[$i]
+    }
+    Write-Host ""
+
+    do
+    {
+        Write-Host ("{0}: " -f $prompt) -NoNewline -foregroundcolor "Cyan"
+        $choice = [int32](Read-Host)
+    }
+    until ($choice -ne '' -and $choice -ge 1 -and $choice -le $options.Count)
+
+    return $options[$choice - 1]
+}
+
+
+# export
+export-modulemember -function PrintSettings
+export-modulemember -function EnterPath
+export-modulemember -function EnterChoice
---
+++ modules/HstwbInstaller-IO.psm1
@@ -0,0 +1,202 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+
+# read zip entry text file
+function ReadZipEntryTextFile($zipFile, $entryName)
+{
+    # open zip archive
+    $zipArchive = [System.IO.Compression.ZipFile]::Open($zipFile,"Read")
+    $zipArchiveEntry = $zipArchive.Entries | Where-Object { $_.FullName -match $entryName } | Select-Object -First 1
+
+    # return null, if zip archive entry doesn't exist
+    if (!$zipArchiveEntry)
+    {
+        $zipArchive.Dispose()
+        return $null
+    }
+
+    # open zip archive entry stream
+    $entryStream = $zipArchiveEntry.Open()
+    $streamReader = New-Object System.IO.StreamReader($entryStream)
+
+    # read text from stream
+    $text = $streamReader.ReadToEnd()
+
+    # close streams
+    $streamReader.Close()
+    $streamReader.Dispose()
+
+    # close zip archive
+    $zipArchive.Dispose()
+    
+    return $text
+}
+
+# read ini file 
+function ReadIniFile($iniFile)
+{
+    return ReadIniText (Get-Content -Path $iniFile)
+}
+
+
+# read ini text
+function ReadIniText($iniText)
+{
+    $ini = @{}
+
+    switch -regex ($iniText -split "`r`n" | Where-Object { $_ })
+    {
+        "^\[(.+)\]$" {
+            $section = $matches[1]
+            $ini[$section] = @{}
+        }
+        "(.+)=(.+)" {
+            $name,$value = $matches[1..2]
+            $ini[$section][$name] = $value
+        }
+    }
+
+    return $ini
+}
+
+
+# write ini file
+function WriteIniFile($iniFile, $ini)
+{
+    $iniLines = @()
+
+    foreach ($key in ($ini.keys | Sort-Object))
+    {
+        if (!($($ini[$key].GetType().Name) -eq "Hashtable"))
+        {
+            $iniLines += "$key=$($ini[$key])"
+        }
+        else
+        {
+            # Section
+            $iniLines += "[$key]"
+            
+            foreach ($sectionKey in ($ini[$key].keys | Sort-Object))
+            {
+                $iniLines += "$sectionKey=$($ini[$key][$sectionKey])"
+            }
+        }
+    }
+
+    [System.IO.File]::WriteAllText($iniFile, $iniLines -join [System.Environment]::NewLine)
+}
+
+
+# calculate md5 hash
+function CalculateMd5($path)
+{
+	$md5 = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+	return [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes($path))).ToLower().Replace('-', '')
+}
+
+
+# get file hashes
+function GetFileHashes($path)
+{
+    $adfFiles = Get-ChildItem -Path $path
+
+    $fileHashes = @()
+
+    foreach ($adfFile in $adfFiles)
+    {
+        $md5Hash = (CalculateMd5 $adfFile.FullName)
+        $fileHashes += @{ "File" = $adfFile.FullName; "Md5Hash" = $md5Hash }
+    }
+
+    return $fileHashes
+}
+
+
+# find matching file hashes
+function FindMatchingFileHashes($hashes, $path)
+{
+    # get file hashes from path
+    $fileHashes = GetFileHashes $path
+
+    # index file hashes
+    $fileHashesIndex = @{}
+    $fileHashes | % { $fileHashesIndex.Set_Item($_.Md5Hash, $_.File) }
+
+    # find files with matching hashes
+    foreach($hash in $hashes)
+    {
+        if ($fileHashesIndex.ContainsKey($hash.Md5Hash))
+        {
+            $hash | Add-Member -MemberType NoteProperty -Name 'File' -Value ($fileHashesIndex.Get_Item($hash.Md5Hash)) -Force
+        }
+    }
+}
+
+
+# read string from bytes
+function ReadString($bytes, $offset, $length)
+{
+	$stringBytes = New-Object 'byte[]' $length 
+	[Array]::Copy($bytes, $offset, $stringBytes, 0, $length)
+	$iso88591 = [System.Text.Encoding]::GetEncoding("ISO-8859-1");
+	return $iso88591.GetString($stringBytes)
+}
+
+
+# read adf disk name
+function ReadAdfDiskName($bytes)
+{
+    # read disk name from offset 0x6E1B0
+    $diskNameOffset = 0x6E1B0
+    $diskNameLength = $bytes[$diskNameOffset]
+
+    ReadString $bytes ($diskNameOffset + 1) $diskNameLength
+}
+
+
+# find matching workbench adfs
+function FindMatchingWorkbenchAdfs($hashes, $path)
+{
+    $adfFiles = Get-ChildItem -Path $path -filter *.adf
+
+    $validWorkbenchAdfFiles = @()
+
+    foreach ($adfFile in $adfFiles)
+    {
+        # read adf bytes
+        $adfBytes = [System.IO.File]::ReadAllBytes($adfFile.FullName)
+
+        if ($adfBytes.Count -eq 901120)
+        {
+            $diskName = ReadAdfDiskName $adfBytes
+            $validWorkbenchAdfFiles += @{ "DiskName" = $diskName; "File" = $adfFile.FullName }
+        }
+    }
+
+
+    # find files with matching disk names
+    foreach($hash in ($hashes | Where { $_.DiskName -ne '' -and !$_.File }))
+    {
+        $workbenchAdfFile = $validWorkbenchAdfFiles | Where { $_.DiskName -eq $hash.DiskName } | Select-Object -First 1
+
+        if (!$workbenchAdfFile)
+        {
+            continue
+        }
+
+        $hash | Add-Member -MemberType NoteProperty -Name 'File' -Value $workbenchAdfFile.File -Force
+    }
+}
+
+
+# export
+export-modulemember -function ReadZipEntryTextFile
+export-modulemember -function ReadIniFile
+export-modulemember -function ReadIniText
+export-modulemember -function WriteIniFile
+export-modulemember -function CalculateMd5
+export-modulemember -function GetFileHashes
+export-modulemember -function FindMatchingFileHashes
+export-modulemember -function ReadString
+export-modulemember -function ReadAdfDiskName
+export-modulemember -function FindMatchingWorkbenchAdfs
