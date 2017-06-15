de25a6f8302ec983c932a47514f9efb8d73ccf2f
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2017-06-15 11:51:40 +0200

    Added initial folder browser dialog

--- hstwb_installer_setup.py
+++ hstwb_installer_setup.py
@@ -12,11 +12,17 @@ def cls():
     """Clear screen"""
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def folder_browser_dialog():
+    """Show folder browser dialog"""
+    print os.name
+
 # clear screen
 cls()
 
 init()
 
-print Style.BRIGHT + Fore.YELLOW + '----------------------------\n'
-print Style.BRIGHT + Fore.YELLOW + 'HstWB Installer Setup v1.1.0\n'
-print Style.BRIGHT + Fore.YELLOW + '----------------------------\n'
+print Style.BRIGHT + Fore.YELLOW + '----------------------------'
+print Style.BRIGHT + Fore.YELLOW + 'HstWB Installer Setup v1.1.0'
+print Style.BRIGHT + Fore.YELLOW + '----------------------------'
+
+folder_browser_dialog()
