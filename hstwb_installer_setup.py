38686125c7508742a0de48b03acfec89474633f9
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2017-06-16 12:20:40 +0200

    Added calculate md5 from file and read csv file

--- hstwb_installer_setup.py
+++ hstwb_installer_setup.py
@@ -6,6 +6,8 @@
 # 2. pip install colorama
 
 import os
+import hashlib
+import csv
 from subprocess import Popen, PIPE
 from sys import platform as _platform
 from colorama import init, Style, Fore
@@ -32,13 +34,36 @@ def folder_dialog():
         proc.wait()
         print "PATH=" + proc.stdout.read().strip()
 
+def calculate_md5_from_file(fname):
+    """Calculate MD5 from file"""
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as _f:
+        for chunk in iter(lambda: _f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+def read_csv_file(fname):
+    """Read CSV File"""
+    reader = csv.DictReader(open(fname, 'rb'), delimiter=';', quotechar='"')
+    rows = []
+    for line in reader:
+        rows.append(line)
+    return rows
+
 # clear screen
 cls()
 
 init()
 
 print Style.BRIGHT + Fore.YELLOW + '----------------------------'
-print Style.BRIGHT + Fore.YELLOW + 'HstWB Installer Setup v1.1.0'
-print Style.BRIGHT + Fore.YELLOW + '----------------------------'
+print 'HstWB Installer Setup v1.1.0'
+print '----------------------------'
+print Style.RESET_ALL
 
-folder_dialog()
+#folder_dialog()
+print calculate_md5_from_file(
+    'c:\\Users\\Public\\Documents\\Amiga Files\\Shared\\rom\\amiga-os-310-a1200.rom')
+
+kickstartRomHashes = read_csv_file('kickstart\\kickstart-rom-hashes.csv') # pylint: disable=invalid-name
+
+print kickstartRomHashes[0]['Name']
