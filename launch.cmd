6571676dabdef870159d479af4e92231801757e8
Henrik Stengaard <hst@firstrealize.com>
2017-09-09 22:46:19 +0200

    Added launch scripts to handle when hstwb installer is being used for the first time

---
+++ launch.cmd
@@ -0,0 +1,44 @@
+@echo off
+:: Launch
+:: ------
+::
+:: Author: Henrik Noerfjand Stengaard
+:: Date:   2017-09-09
+::
+:: A Windows batch script to launch HstWB Installer.
+
+:: variables
+set TASK=%1
+set SETTINGSDIR=%LOCALAPPDATA%\HstWB Installer
+
+:: run first time script and goto setup, if it's first time hstwb installer is used
+powershell -ExecutionPolicy Bypass -File first-time.ps1 -settingsDir "%SETTINGSDIR%"
+IF %ERRORLEVEL% == 1 GOTO setup
+
+:: goto task
+IF "%TASK%" == "settings" GOTO settings
+IF "%TASK%" == "assigns" GOTO assigns
+IF "%TASK%" == "setup" GOTO setup
+IF "%TASK%" == "run" GOTO run
+GOTO end
+
+:: edit settings
+:settings
+notepad "%SETTINGSDIR%\hstwb-installer-settings.ini"
+GOTO END
+
+:: edit assigns
+:assigns
+notepad "%SETTINGSDIR%\hstwb-installer-assigns.ini"
+GOTO END
+
+:: run hstwb installer setup script
+:setup
+powershell -ExecutionPolicy Bypass -File hstwb-installer-setup.ps1 -settingsDir "%SETTINGSDIR%"
+GOTO END
+
+:: run hstwb installer run script
+:run
+powershell -ExecutionPolicy Bypass -File hstwb-installer-run.ps1 -settingsDir "%SETTINGSDIR%"
+
+:end
