501bb97103e52ff04aa08dc99d66e01f73a5774a
Henrik Stengaard <hst@firstrealize.com>
2017-11-02 22:04:23 +0100

    Renamed install uae config files

--- support/Install UAE Config/install_uae-config_patch_only.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -File install_uae-config.ps1 -patchOnly
-pause
---
+++ support/Install UAE Config/install_uae_config_patch_only.cmd
@@ -0,0 +1,3 @@
+@echo off
+powershell -ExecutionPolicy Bypass -File install_uae_config.ps1 -patchOnly
+pause
