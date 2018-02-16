6d75fcb374cb6eac2ab372ca0d53ddc1939c1c04
Henrik Stengaard <hst@firstrealize.com>
2018-02-16 21:05:33 +0100

    Updated rdbinfo python script to read and partitions

--- scripts/rdbinfo.py
+++ scripts/rdbinfo.py
@@ -15,10 +15,15 @@ Script: RDB Info v1.0
 """
 
 from __future__ import print_function
+from subprocess import Popen, PIPE
+from sys import platform,argv
+import os
 import ctypes
 import ctypes.wintypes
 import stat as stdstat
 import struct
+import re
+import math
 
 class Win32DiskReader(object):
     """
@@ -179,9 +184,30 @@ class PartitionBlock:
     dev_flags = 0 # Preferred flags for OpenDevice
     drive_name = "" # Preferred DOS device name: BSTR form
 
-class HdfDiskReader(object):
-    def __init__(self, reader):
-        self.reader = reader
+    size_of_vector = 0 # Size of Environment vector
+    size_block = 0 # Size of the blocks in 32 bit words, usually 128
+    sec_org = 0 # Not used; must be 0
+    surfaces = 0 # Number of heads (surfaces)
+    sectors_per_block = 0 # Disk sectors per block, used with SizeBlock, usually 1
+    blocks_per_track = 0 # Blocks per track. drive specific
+    reserved = 0 # DOS reserved blocks at start of partition.
+    pre_alloc = 0 # DOS reserved blocks at end of partition
+    interleave = 0 # Not used, usually 0
+    low_cyl	= 0 # First cylinder of the partition
+    high_cyl = 0 # Last cylinder of the partition
+    num_buffer = 0 # Initial # DOS of buffers.
+    buf_mem_type = 0 # Type of mem to allocate for buffers
+    max_transfer = 0 # Max number of bytes to transfer at a time
+    mask = 0 # Address Mask to block out certain memory
+    boot_priority = 0 # Boot priority for autoboot
+    dos_type = 0 # Dostype of the file system
+    baud = 0 # Baud rate for serial handler
+    control = 0 # Control word for handler/filesystem 
+    boot_blocks = 0 # Number of blocks containing boot code 
+
+class RigidDiskReader(object):
+    def __init__(self, disk):
+        self.disk = disk
 
         # constants
         self.BLOCK_SIZE = 512
@@ -206,8 +232,8 @@ class HdfDiskReader(object):
     def find_rigid_disk_block(self):
         for block_index in range(0, self.RDB_LOCATION_LIMIT):
             block_offset = self.BLOCK_SIZE * block_index
-            self.reader.seek(block_offset)
-            block_data = self.reader.read(self.BLOCK_SIZE)
+            self.disk.seek(block_offset)
+            block_data = self.disk.read(self.BLOCK_SIZE)
             identifier = self.read_identifier(block_data, 0)
             if identifier == "RDSK":
                 return block_data
@@ -272,6 +298,27 @@ class HdfDiskReader(object):
         partition_block.dev_flags = self.read_unsigned_long(block_data, 32)
         partition_block.drive_name = self.read_string(block_data, 36)
 
+        partition_block.size_of_vector = self.read_unsigned_long(block_data, 128)
+        partition_block.size_block = self.read_unsigned_long(block_data, 132)
+        partition_block.sec_org = self.read_unsigned_long(block_data, 136)
+        partition_block.surfaces = self.read_unsigned_long(block_data, 140)
+        partition_block.sectors_per_block = self.read_unsigned_long(block_data, 144)
+        partition_block.blocks_per_track = self.read_unsigned_long(block_data, 148)
+        partition_block.reserved = self.read_unsigned_long(block_data, 152)
+        partition_block.pre_alloc = self.read_unsigned_long(block_data, 156)
+        partition_block.interleave = self.read_unsigned_long(block_data, 160)
+        partition_block.low_cyl = self.read_unsigned_long(block_data, 164)
+        partition_block.high_cyl = self.read_unsigned_long(block_data, 168)
+        partition_block.num_buffer = self.read_unsigned_long(block_data, 172)
+        partition_block.buf_mem_type = self.read_unsigned_long(block_data, 176)
+        partition_block.max_transfer = self.read_unsigned_long(block_data, 180)
+        partition_block.mask = self.read_unsigned_long(block_data, 184)
+        partition_block.boot_priority = self.read_unsigned_long(block_data, 188)
+        partition_block.dos_type = self.read_char(block_data, 192, 4)
+        partition_block.baud = self.read_unsigned_long(block_data, 196)
+        partition_block.control = self.read_unsigned_long(block_data, 200)
+        partition_block.boot_blocks = self.read_unsigned_long(block_data, 204)
+
         return partition_block
 
     def read_partition_list(self, rigid_disk_block):
@@ -280,8 +327,8 @@ class HdfDiskReader(object):
         partition_index = 1
         while True:
             partition_block_offset = rigid_disk_block.block_size * partition_list
-            self.reader.seek(partition_block_offset)
-            block_data = self.reader.read(self.BLOCK_SIZE)
+            self.disk.seek(partition_block_offset)
+            block_data = self.disk.read(self.BLOCK_SIZE)
             partition_block = self.read_partition_block(block_data)
             partition_blocks.append(partition_block)
             partition_list = partition_block.next_partition_block
@@ -290,30 +337,102 @@ class HdfDiskReader(object):
             partition_index += 1
         return partition_blocks
 
-DISK_READER = Win32DiskReader()
-DISK_READER.open("c:\\temp\\hdf_repack\\4gb.hdf")
-HDF_DISK_READER = HdfDiskReader(DISK_READER)
+def format_bytes(size, precision = 0):
+    base = math.log(size, 1024)
+    units = ["", "K", "M", "G", "T"]
+    return "%s %sB" % (round(math.pow(1024, base - math.floor(base)), precision), units[int(math.floor(base))])
 
-rigid_disk_block = HDF_DISK_READER.read_rigid_disk_block()
-partition_blocks = HDF_DISK_READER.read_partition_list(rigid_disk_block)
 
-# calculate drive size
-DRIVE_SIZE = rigid_disk_block.cylinders * rigid_disk_block.heads * rigid_disk_block.sectors * HDF_DISK_READER.BLOCK_SIZE
+# print usage and exit, if arguments are not defined
+if len(argv) <= 1:
+    print ("Usage: %s \"[PATH]\"" % argv[0])
+    exit()
 
+# get path
+path = argv[1].strip()
+
+# set disk path and change disk path to logical disk, if it matches drive letter
+disk_path = path
+if platform == "win32" and re.match(r'^[a-z]:$', disk_path, re.M|re.I):
+    disk_path = "\\\\.\\%s" % disk_path 
+
+# default disk size
+disk_size = -1
+
+# open disk path and read rigid disk blocks
+with open(disk_path, 'rb') as disk:
+    # get disk size
+    # use powershell to get partition size, if platform is win32 and path is not a file
+    if platform == "win32" and not os.path.isfile(path):
+        drive_letter = None
+        drive_letter_match = re.match(r'^([a-z]):$', path, re.M|re.I)
+        logical_disk_match = re.match(r'\\\\\.\\([a-z]):$', path, re.M|re.I)
+        if drive_letter_match:
+            drive_letter = drive_letter_match.group(1)
+        elif logical_disk_match:
+            drive_letter = logical_disk_match.group(1)
+
+        if drive_letter:
+            cmd = "powershell -ExecutionPolicy Bypass -Command \"(Get-Partition -DriveLetter '%s').Size\"" % drive_letter
+            proc = Popen(cmd, stdout=PIPE)
+            proc.wait()
+            disk_size = int(proc.stdout.read().strip())
+    else:
+        disk.seek(0, 2)
+        disk_size = disk.tell()
+
+    # read rigid disk blocks
+    rigid_disk_reader = RigidDiskReader(disk)
+    rigid_disk_block = rigid_disk_reader.read_rigid_disk_block()
+    partition_blocks = rigid_disk_reader.read_partition_list(rigid_disk_block)
+
+    # calculate drive size
+    drive_size = rigid_disk_block.cylinders * rigid_disk_block.heads * rigid_disk_block.sectors * rigid_disk_reader.BLOCK_SIZE
+
+
+print ("Disk:")
+print ("-----")
+print ("Path:                     %s" % path)
+
+if disk_size > 0:
+    print ("Size:                     %s (%d bytes)" % (format_bytes(disk_size, 1), disk_size))
+
+# show physical drive
+print ("")
 print ("Physical drive:")
 print ("---------------")
-print ("Manufacturers Name = %s" % rigid_disk_block.disk_vendor)
-print ("Drive Name = %s" % rigid_disk_block.disk_product)
-print ("Drive Revision = %s" % rigid_disk_block.disk_revision)
+print ("Manufacturers Name:       %s" % rigid_disk_block.disk_vendor)
+print ("Drive Name:               %s" % rigid_disk_block.disk_product)
+print ("Drive Revision:           %s" % rigid_disk_block.disk_revision)
 print ("")
-print ("Cylinders:          %s" % rigid_disk_block.cylinders)
-print ("Heads:              %s" % rigid_disk_block.sectors)
-print ("Blocks per Track:   %s" % rigid_disk_block.heads)
-print ("Size:   %d" % DRIVE_SIZE)
+print ("Cylinders:                %s" % rigid_disk_block.cylinders)
+print ("Heads:                    %s" % rigid_disk_block.sectors)
+print ("Blocks per Track:         %s" % rigid_disk_block.heads)
+print ("Size:                     %d" % drive_size)
+print ("Size:                     %s (%d bytes)" % (format_bytes(drive_size, 1), drive_size))
 
+# show partitions
+partition_index = 0
 for partition_block in partition_blocks:
-    print ("host id:          %s" % partition_block.host_id)
-    print ("next block:          %s" % partition_block.next_partition_block)
-    print ("drive name:          %s" % partition_block.drive_name)
+    partition_index += 1
 
-DISK_READER.close()
+    # calculate partition size
+    partition_cylinders = partition_block.high_cyl - partition_block.low_cyl + 1
+    partition_size = partition_cylinders * partition_block.surfaces * partition_block.blocks_per_track * rigid_disk_block.block_size
+    
+    dos_type_formatted = "%s\%d" % (struct.unpack_from('3s', partition_block.dos_type, 0)[0], struct.unpack_from('B', partition_block.dos_type, 3)[0])
+
+
+    print ("")
+    print ("Partition %d:" % partition_index)
+    print ("------------")
+    print ("Device Name:              %s" % partition_block.drive_name)
+    print ("Start Cylinder:           %d" % partition_block.low_cyl)
+    print ("End Cylylinder:           %d" % partition_block.high_cyl)
+    print ("Total Cylinder:           %d" % partition_cylinders)
+    print ("Buffers:                  %d" % partition_block.num_buffer)
+    print ("File System Block Size:   %d" % (partition_block.size_block * 4 * partition_block.sectors_per_block))
+    print ("Mask:                     %s" % hex(partition_block.mask).upper())
+    print ("Max Transfer:             %s, (%d)" % (hex(partition_block.max_transfer).upper(), partition_block.max_transfer))
+    print ("Dos Type:                 %s" % (dos_type_formatted))
+    print ("Size:                     %s (%d bytes)" % (format_bytes(partition_size, 1), partition_size))
