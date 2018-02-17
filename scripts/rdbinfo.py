993030c69ae8b293e24231d6e538e9723f0b74d3
Henrik Stengaard <hst@firstrealize.com>
2018-02-17 20:13:15 +0100

    Updated rdb info python script to show partition flags and dos type properly. Moved unused win32 disk reader separate script

--- scripts/rdbinfo.py
+++ scripts/rdbinfo.py
@@ -3,12 +3,24 @@
 #
 # RDB Info
 # Author: Henrik Noerfjand Stengaard
-# Date: 2018-02-15
+# Date: 2018-02-17
 #
-# http://pydoc.net/watchdog/0.8.2/watchdog.utils.win32stat/
-# https://github.com/maxpat78/FATtools/blob/master/disk.py
-# http://pydoc.net/RSFile/1.1/rsbackends.pywin32_ctypes/
-# 
+# A python script to read Rigid Disk Block (RDB) from file or disk device.
+#
+# Examples:
+# ---------
+# Read RDB from hdf file on Windows platform
+# python rdbinfo.py "c:\temp\4gb.hdf"
+#
+# Read RDB from drive letter F: on Windows platform
+# python rdbinfo.py "\\.\F:"
+#
+# Read RDB from hdf file on macOS or Linux platform
+# python rdbinfo.py "/tmp/4gb.hdf"
+#
+# Read RDB form disk device on macOS or Linux platform
+# python rdbinfo.py "/dev/sdb"
+
 
 """
 Script: RDB Info v1.0
@@ -18,121 +30,10 @@ from __future__ import print_function
 from subprocess import Popen, PIPE
 from sys import platform,argv
 import os
-import ctypes
-import ctypes.wintypes
 import stat as stdstat
 import struct
 import re
 import math
-
-class Win32DiskReader(object):
-    """
-    Constructor
-    """
-    def __init__(self):
-        # constants
-        self.INVALID_HANDLE_VALUE = ctypes.c_void_p(-1).value
-        self.OPEN_EXISTING = 3
-        self.GENERIC_READ = 0x80000000
-        self.FILE_READ_ATTRIBUTES = 0x80
-        self.FILE_ATTRIBUTE_NORMAL = 0x80
-        self.FILE_ATTRIBUTE_READONLY = 0x1
-        self.FILE_ATTRIBUTE_DIRECTORY = 0x10
-        self.FILE_FLAG_BACKUP_SEMANTICS = 0x02000000
-        self.FILE_FLAG_OPEN_REPARSE_POINT = 0x00200000
-        self.FILE_FLAG_NO_BUFFERING = 0x20000000
-
-        # kernel32 create file
-        self.CreateFile = ctypes.windll.kernel32.CreateFileW
-        self.CreateFile.restype = ctypes.wintypes.HANDLE
-        self.CreateFile.argtypes = (
-            ctypes.c_wchar_p,
-            ctypes.wintypes.DWORD,
-            ctypes.wintypes.DWORD,
-            ctypes.c_void_p,
-            ctypes.wintypes.DWORD,
-            ctypes.wintypes.DWORD,
-            ctypes.wintypes.HANDLE,
-        )
-
-        # kernel32 read file
-        self.ReadFile = ctypes.windll.kernel32.ReadFile        
-        self.ReadFile.restype = ctypes.wintypes.BOOL
-        self.ReadFile.argtypes = (
-            ctypes.wintypes.HANDLE,
-            ctypes.wintypes.LPVOID,
-            ctypes.wintypes.DWORD,
-            ctypes.POINTER(ctypes.wintypes.DWORD),
-            ctypes.wintypes.LPVOID)
-
-        # kernel32 set file pointer ex
-        self.SetFilePointerEx = ctypes.windll.kernel32.SetFilePointerEx
-        self.SetFilePointerEx.restype = ctypes.wintypes.BOOL
-        self.SetFilePointerEx.argtypes = (
-            ctypes.wintypes.HANDLE,
-            ctypes.c_longlong,
-            ctypes.POINTER(ctypes.c_longlong),
-            ctypes.wintypes.DWORD)
-
-        # kernel32 close handle
-        self.CloseHandle = ctypes.windll.kernel32.CloseHandle
-        self.CloseHandle.restype = ctypes.wintypes.BOOL
-        self.CloseHandle.argtypes = (ctypes.wintypes.HANDLE,)
-
-        self.handle = None
-        self._pos = 0
-    """
-    Open file
-    """
-    def open(self, path):
-        self.handle = self.CreateFile(path,
-            self.GENERIC_READ,
-            0x1,
-            None,
-            self.OPEN_EXISTING,
-            self.FILE_FLAG_NO_BUFFERING,
-            None)
-        if self.handle == self.INVALID_HANDLE_VALUE:
-            raise ctypes.WinError()
-
-    """
-    Read
-    """
-    def read(self, length):
-        data = ctypes.create_string_buffer(length)
-        bytes_read = ctypes.wintypes.DWORD(0)            
-        ret = self.ReadFile(
-            self.handle,
-            data,
-            length,
-            ctypes.byref(bytes_read),
-            None)
-        if ret == 0:
-            raise ctypes.WinError()
-        return data
-
-    """
-    Seek
-    """
-    def seek(self, offset, whence=0):
-        if whence == 0:
-            npos = offset
-        elif whence == 1:
-            npos = self._pos + offset
-        else:
-            npos = self._pos - offset
-        if self._pos == npos:
-            return
-        n = ctypes.c_longlong(offset)
-        if 0xFFFFFFFF == self.SetFilePointerEx(self.handle, offset&0xFFFFFFFF, ctypes.byref(n), whence):
-            raise ctypes.WinError()
-        self._pos = npos
-
-    """
-    Close
-    """
-    def close(self):
-        self.CloseHandle(self.handle)
 
 class RigidDiskBlock:
     size = 0 # Size of the structure for checksums
@@ -408,7 +309,6 @@ print ("")
 print ("Cylinders:                %s" % rigid_disk_block.cylinders)
 print ("Heads:                    %s" % rigid_disk_block.sectors)
 print ("Blocks per Track:         %s" % rigid_disk_block.heads)
-print ("Size:                     %d" % drive_size)
 print ("Size:                     %s (%d bytes)" % (format_bytes(drive_size, 1), drive_size))
 
 # show partitions
@@ -420,8 +320,8 @@ for partition_block in partition_blocks:
     partition_cylinders = partition_block.high_cyl - partition_block.low_cyl + 1
     partition_size = partition_cylinders * partition_block.surfaces * partition_block.blocks_per_track * rigid_disk_block.block_size
     
-    dos_type_formatted = "%s\%d" % (struct.unpack_from('3s', partition_block.dos_type, 0)[0], struct.unpack_from('B', partition_block.dos_type, 3)[0])
-
+    dos_type_formatted = "%s\\%02d" % (struct.unpack_from('3s', partition_block.dos_type, 0)[0], struct.unpack_from('B', partition_block.dos_type, 3)[0])
+    dos_type = hex(struct.unpack_from('>L', partition_block.dos_type, 0)[0])
 
     print ("")
     print ("Partition %d:" % partition_index)
@@ -430,9 +330,19 @@ for partition_block in partition_blocks:
     print ("Start Cylinder:           %d" % partition_block.low_cyl)
     print ("End Cylylinder:           %d" % partition_block.high_cyl)
     print ("Total Cylinder:           %d" % partition_cylinders)
+    print ("Size:                     %s (%d bytes)" % (format_bytes(partition_size, 1), partition_size))
     print ("Buffers:                  %d" % partition_block.num_buffer)
     print ("File System Block Size:   %d" % (partition_block.size_block * 4 * partition_block.sectors_per_block))
-    print ("Mask:                     %s" % hex(partition_block.mask).upper())
-    print ("Max Transfer:             %s, (%d)" % (hex(partition_block.max_transfer).upper(), partition_block.max_transfer))
-    print ("Dos Type:                 %s" % (dos_type_formatted))
-    print ("Size:                     %s (%d bytes)" % (format_bytes(partition_size, 1), partition_size))
+
+    # bootable partition flag
+    if partition_block.flags & 0x1:
+        print ("Bootable")
+        print ("Boot Priority:            %d" % partition_block.boot_priority)
+
+    # no mount partition flag
+    if partition_block.flags & 0x2:
+        print ("No Automount")
+
+    print ("Mask:                     %s" % hex(partition_block.mask))
+    print ("Max Transfer:             %s, (%d)" % (hex(partition_block.max_transfer), partition_block.max_transfer))
+    print ("Dos Type:                 %s (%s)" % (dos_type, dos_type_formatted))
