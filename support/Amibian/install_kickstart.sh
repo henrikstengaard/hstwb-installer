dc54f7990b251173c0a4936803f12c8b8e58ce6a
Henrik Stengaard <hst@firstrealize.com>
2018-04-03 11:40:49 +0200

    Updated install kickstart script

--- support/Amibian/install_kickstart.sh
+++ support/Amibian/install_kickstart.sh
@@ -8,6 +8,16 @@
 # A bash script to find and install A1200 Kickstart 3.1 (40.068) rom file in install directory and install it in kickstarts directory.
 
 
+# kickstarts directory
+kickstartsdir="/root/amibian/amiga_files/kickstarts"
+
+# install directory
+installdir="/media/usb0/kickstart"
+
+# install
+install=1
+
+
 # install a1200 kickstart 3.1 rom
 function install()
 {
@@ -17,13 +27,11 @@ function install()
 		return 1
 	fi
 
-	clear
-	echo "Finding A1200 Kickstart 3.1 (40.068) rom in install directory '$installdir'..."
+	# set installed false
+	installed=1
 
 	# find a1200 kickstart 3.1 rom files in install directory
-	find "$installdir" -type f | while read romfile; do
-		echo "$romfile"
-
+	while read romfile; do
 		# get md5 from rom file
 		md5=$(md5sum "$romfile" | awk '{ print $1 }')
 
@@ -36,35 +44,43 @@ function install()
 			# show error dialog, if cloanto amiga forever kickstart key file doesn't exist
 			if [ ! -f "$romkeyfile" ]; then
 				dialog --clear --title "ERROR" --msgbox "Cloanto Amiga Forever Kickstart key file 'rom.key' doesn't exist in install directory '$installdir'." 0 0
-				return 1
+				break
 			fi
 
+			# copy rom and key file to kickstarts directory
 			cp "$romfile" "$kickstartsdir/kick31.rom"
 			cp "$romkeyfile" "$kickstartsdir/rom.key"
 
+			# set installed true
+			installed=0
+
 			# show success dialog
 			dialog --clear --title "Success" --msgbox "Successfully installed Cloanto Amiga Forever Kickstart 3.1 (40.068) rom in kickstarts directory '$kickstartsdir'" 0 0
-
-			return 0
+			break
 		fi
 
 		# copy rom file, if md5 matches original kickstart 3.1 (40.068) (a1200) rom
 		if [ "$md5" == "646773759326fbac3b2311fd8c8793ee" ]; then
+			# copy rom file to kickstarts directory
 			cp "$romfile" "$kickstartsdir/kick31.rom"
+
+			# set installed true
+			installed=0
 
 			# show success dialog
 			dialog --clear --title "Success" --msgbox "Successfully installed Original Kickstart 3.1 (40.068) rom in kickstarts directory '$kickstartsdir'" 0 0
-
-			return 0
+			break
 		fi
-	done
+	done < <(find "$installdir" -type f)
 
-	echo "Done."
+	# show error dialog, if kickstart rom is not installed
+	if [ $installed != 0 ]; then
+		# show success dialog
+		dialog --clear --title "ERROR" --msgbox "A1200 Kickstart 3.1 (40.068) rom doesn't exist in install directory '$installdir'" 0 0
+		return 1
+	fi
 
-	# show success dialog
-	dialog --clear --title "ERROR" --msgbox "A1200 Kickstart 3.1 (40.068) rom doesn't exist in install directory '$installdir'" 0 0
-
-	return 1
+	return 0
 }
 
 
@@ -84,16 +100,6 @@ function change_install_directory()
 
 	return 0
 }
-
-
-# kickstarts directory
-kickstartsdir="/root/amibian/amiga_files/kickstarts"
-
-# install directory
-installdir="/media/usb0/kickstart"
-
-# install
-install=1
 
 
 # parse arguments
@@ -118,21 +124,23 @@ case $i in
 esac
 done
 
+# install, if install argument is present
+if [ $install == 0 ]; then
+	# exit, if kick31.rom already exists
+	if [ -f "$kickstartsdir/kick31.rom" ]; then
+		exit
+	fi
 
-# exit, if kick31.rom already exists
-if [ -f "$kickstartsdir/kick31.rom" ]; then
-	exit
-fi
-
-# exit, if install argument is present and install is successful
-if [ $install == 0 ] && install; then
-	exit
+	# exit, if install is successful
+	if install; then
+		exit
+	fi
 fi
 
 # install kickstart menu
 while true; do
 	# show install kickstart menu
-	choices=$(dialog --clear --stdout --title "Install Kickstart" --menu "Find and install A1200 Kickstart 3.1 (40.068) rom from install directory '$installdir':" 0 0 0 1 "Install Kickstart rom" 2 "Change Install Directory")
+	choices=$(dialog --clear --stdout --title "Install Kickstart" --menu "Find and install A1200 Kickstart 3.1 (40.068) rom from install directory '$installdir':" 0 0 0 1 "Install Kickstart rom" 2 "Change Install Directory" 3 "Exit")
 
 	# exit, if cancelled
 	if [ $? -ne 0 ]; then
@@ -144,10 +152,16 @@ while true; do
     for choice in $choices; do
         case $choice in
         1)
-			install
+			# exit, if install is successful
+			if install; then
+				exit
+			fi
 			;;
         2)
 			change_install_directory
+			;;
+        3)
+			exit
 			;;
 		esac
 		clear
