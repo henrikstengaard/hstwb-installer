387deaa4c935d2a2915277fdbb666c579e8e5dc8
Henrik Stengaard <hst@firstrealize.com>
2018-04-07 21:33:35 +0200

    Updated make installer script to build portable zip and msi releases

--- tools/make_installer.ps1
+++ tools/make_installer.ps1
@@ -4,7 +4,7 @@
 # A powershell script to make a msi installer for HstWB Installer.
 #
 # Author: Henrik Noerfjand Stengaard
-# Date:   2017-09-19
+# Date:   2018-04-07
 
 # Requirements:
 # - Pandoc
@@ -14,7 +14,7 @@
 # WiX Toolset is used to build a msi installer and can be downloaded here http://wixtoolset.org/releases/.
 
 # Running msi installer with logging:
-# msiexec /i hstwb-installer.1.0.0.msi /L*V "install.log"
+# msiexec /i hstwb-installer.msi /L*V "install.log"
 
 
 Import-Module (Resolve-Path('..\modules\version.psm1')) -Force
@@ -22,74 +22,19 @@ Import-Module (Resolve-Path('..\modules\config.psm1')) -Force
 Import-Module (Resolve-Path('..\modules\data.psm1')) -Force
 
 
-# start process
-function StartProcess($fileName, $arguments, $workingDirectory)
-{
-	# start process info
-	$processInfo = New-Object System.Diagnostics.ProcessStartInfo
-	$processInfo.FileName = $fileName
-	$processInfo.RedirectStandardError = $true
-	$processInfo.RedirectStandardOutput = $true
-	$processInfo.UseShellExecute = $false
-	$processInfo.Arguments = $arguments
-	$processInfo.WorkingDirectory = $workingDirectory
-	$processInfo.CreateNoWindow = $true
-
-    # Creating string builders to store stdout and stderr.
-    $oStdOutBuilder = New-Object -TypeName System.Text.StringBuilder
-    $oStdErrBuilder = New-Object -TypeName System.Text.StringBuilder
-
-	# run process
-	$process = New-Object System.Diagnostics.Process
-	$process.StartInfo = $processInfo
-
-    $sScripBlock = {
-        if (! [String]::IsNullOrEmpty($EventArgs.Data)) {
-            $Event.MessageData.AppendLine($EventArgs.Data)
-        }
-    }
-	
-	$oStdOutEvent = Register-ObjectEvent -InputObject $process `
-        -Action $sScripBlock -EventName 'OutputDataReceived' `
-        -MessageData $oStdOutBuilder
-    $oStdErrEvent = Register-ObjectEvent -InputObject $process `
-        -Action $sScripBlock -EventName 'ErrorDataReceived' `
-        -MessageData $oStdErrBuilder
-
-
-	$process.Start() | Out-Null
-    $process.BeginErrorReadLine()
-    $process.BeginOutputReadLine()
-	$process.WaitForExit()
-
-    # Unregistering events to retrieve process output.
-    Unregister-Event -SourceIdentifier $oStdOutEvent.Name
-    Unregister-Event -SourceIdentifier $oStdErrEvent.Name
-
-	if ($process.ExitCode -ne 0)
-	{
-		if ($oStdOutBuilder.Length -gt 0)
-		{
-			Write-Host $oStdOutBuilder.ToString()
-		}
-
-		if ($oStdErrBuilder.Length -gt 0)
-		{
-			Write-Host $oStdErrBuilder.ToString()
-		}
-
-        Write-Error ("Failed to run '" + $fileName + "' with arguments '$arguments' returned error code " + $process.ExitCode)
-
-        exit 1
-	}
-}
-
+# convert markdown to html
 function ConvertMarkdownToHtml($pandocFile, $githubPandocFile, $markdownFile, $htmlFile)
 {
 	# build readme html from readme markdown using pandoc
 	$pandocArgs = "-f markdown_github -c ""$githubPandocFile"" -t html5 ""$markdownFile"" -o ""$htmlFile"""
-	StartProcess $pandocFile $pandocArgs (Split-Path $markdownFile -Parent)
-
+	$pandocProcess = Start-Process $pandocFile -ArgumentList $pandocArgs -WorkingDirectory (Split-Path $markdownFile -Parent) -Wait -NoNewWindow -PassThru
+	
+	if ($pandocProcess.ExitCode -ne 0)
+	{
+		Write-Host ("Error: Pandoc failed with exit code {0}!" -f $pandocProcess.ExitCode) -ForegroundColor 'Red'
+		exit 1
+	}
+	
 	# read github pandoc css and html
 	$githubPandocCss = [System.IO.File]::ReadAllText($githubPandocFile)
 	$html = [System.IO.File]::ReadAllText($htmlFile)
@@ -109,8 +54,15 @@ $wixToolsetHeatFile = Join-Path $wixToolsetDir -ChildPath 'heat.exe'
 $wixToolsetCandleFile = Join-Path $wixToolsetDir -ChildPath 'candle.exe'
 $wixToolsetLightFile = Join-Path $wixToolsetDir -ChildPath 'light.exe'
 $rootDir = Resolve-Path '..'
-$outputDir = Join-Path $rootDir -ChildPath '.output'
+$releaseDir = Join-Path $rootDir -ChildPath '.release'
+$buildDir = Join-Path $releaseDir -ChildPath '.build'
+$components = @("Amiga", "Fonts", "Fs-Uae", "Images", "Kickstart", "Licenses", "Modules", "Readme", "Scripts", "Support", "Winuae", "Workbench" )
 
+
+Write-Output "Build Release"
+Write-Output "-------------"
+Write-Output ""
+Write-Output ("Release: 'HstWB Installer v{0}'"-f $hstwbInstallerVersion.ToLower())
 
 # fail, if pandoc file doesn't exist
 if (!(Test-Path -path $pandocFile))
@@ -127,18 +79,69 @@ if (!(Test-Path -path $wixToolsetDir))
 }
 
 
-# remove output directory, if it exists
-if (Test-Path -Path $outputDir)
+# remove release directory, if it exists
+if (Test-Path -Path $releaseDir)
 {
-    Remove-Item -Path $outputDir -Recurse -Force
+    Remove-Item -Path $releaseDir -Recurse -Force
 }
 
-# create output directory
-mkdir -Path $outputDir | Out-Null
+# create release directory
+mkdir -Path $releaseDir | Out-Null
+
+# create build directory
+mkdir -Path $buildDir | Out-Null
 
 
-# Build readme files
-# ------------------
+# build hstwb installer
+# ---------------------
+
+Write-Output ""
+Write-Output "Build HstWB Installer"
+Write-Output "---------------------"
+
+# copy application
+Write-Output "- Copying application files..."
+
+Copy-Item -Path (Resolve-Path '..\install.*') -Recurse -Destination $buildDir
+Copy-Item -Path (Resolve-Path '..\launcher.*') -Recurse -Destination $buildDir
+Copy-Item -Path (Resolve-Path '..\setup.*') -Recurse -Destination $buildDir
+Copy-Item -Path (Resolve-Path '..\run.*') -Recurse -Destination $buildDir
+Copy-Item -Path (Resolve-Path '..\LICENSE.txt') -Recurse -Destination $buildDir
+Copy-Item -Path (Resolve-Path '..\hstwb_installer.ico') -Recurse -Destination $buildDir
+
+# update year in license txt file
+$licenseTxtFile = Join-Path $buildDir -ChildPath 'LICENSE.txt'
+$licenseTxtText = [System.IO.File]::ReadAllText($licenseTxtFile) -replace 'Copyright \(c\) \d+', ("Copyright (c) {0}" -f [System.DateTime]::Now.Year)
+[System.IO.File]::WriteAllText($licenseTxtFile, $licenseTxtText)
+
+# copy componenet directories
+foreach($component in $components)
+{
+	$componentDir = Join-Path -Path $rootDir -ChildPath $component
+
+	if (!(Test-Path $componentDir))
+	{
+		continue
+	}
+
+	Copy-Item -Path $componentDir -Recurse -Destination $buildDir
+}
+
+
+# copy packages
+Write-Output "- Copying packages files..."
+
+$packagesPath = Join-Path -Path $rootDir -ChildPath 'Packages'
+$packageFiles = @()
+$packageFiles += Get-ChildItem $packagesPath\* -Include *.zip
+
+$buildPackagesPath = Join-Path $buildDir -ChildPath 'Packages'
+mkdir -Path $buildPackagesPath | Out-Null
+$packageFiles | ForEach-Object { Copy-Item -Path $_.FullName -Destination $buildPackagesPath }
+
+
+# build readme
+Write-Output "- Building readme html files..."
 
 $readmeMarkdownLines = @()
 $readmeMarkdownLines += "# Readme"
@@ -148,9 +151,7 @@ $readmeMarkdownLines += ""
 $readmeMarkdownLines += "Readme for HstWB Installer:"
 $readmeMarkdownLines += "* [HstWB Installer](HstWB Installer/readme.html)"
 
-Write-Host "Building readme html from github markdown..." -ForegroundColor 'Yellow'
-
-$readmeDir = Join-Path $outputDir -ChildPath 'Readme'
+$readmeDir = Join-Path $buildDir -ChildPath 'Readme'
 mkdir -Path $readmeDir | Out-Null
 
 $hstwbInstallerReadmeDir = Join-Path $readmeDir -ChildPath 'HstWB Installer'
@@ -167,23 +168,9 @@ ConvertMarkdownToHtml $pandocFile $githubPandocFile $hstwbInstallerReadmeMarkdow
 $screenshotsDir = Join-Path -Path $rootDir -ChildPath 'Screenshots'
 Copy-Item $screenshotsDir -Destination $hstwbInstallerReadmeDir -Recurse
 
-Write-Host "Done." -ForegroundColor 'Green'
 
-# Copy packages component directory
-# ---------------------------------
-
-Write-Host "Copying packages component directory..." -ForegroundColor 'Yellow'
-
-$packagesPath = Join-Path -Path $rootDir -ChildPath 'Packages'
-$packageFiles = @()
-$packageFiles += Get-ChildItem $packagesPath\* -Include *.zip
-
-$outputPackagesPath = Join-Path $outputDir -ChildPath 'Packages'
-mkdir -Path $outputPackagesPath | Out-Null
-$packageFiles | ForEach-Object { Copy-Item -Path $_.FullName -Destination $outputPackagesPath }
-
-Write-Host "Done." -ForegroundColor 'Green'
-
+# extract read html files from packages
+Write-Output "- Extracting readme html files from packages..."
 
 # Copy packages readme and screenshots
 $packagesReadmeDir = Join-Path $readmeDir -ChildPath 'Packages'
@@ -239,114 +226,91 @@ foreach($packageFile in $packageFiles)
 }
 
 # write readme markdown file
-$readmeMarkdownFile = Join-Path $outputDir -ChildPath 'README.md'
+$readmeMarkdownFile = Join-Path $buildDir -ChildPath 'README.md'
 Set-Content -path $readmeMarkdownFile -Value $readmeMarkdownLines -Encoding UTF8
 
 # convert readme markdown file to html
 $readmeHtmlFile = Join-Path $readmeDir -ChildPath 'README.html'
 ConvertMarkdownToHtml $pandocFile $githubPandocFile $readmeMarkdownFile $readmeHtmlFile
 
+Write-Output "Done."
+Write-Output ("Successfully build HstWB Installer directory '{0}'." -f $buildDir)
+Write-Output ""
 
-# Copy other component directories
-# --------------------------------
 
-Write-Host "Copying component directories..." -ForegroundColor 'Yellow'
+# build portable release
+# ----------------------
 
-$components = @("Amiga", "Fonts", "Fs-Uae", "Images", "Kickstart", "Licenses", "Modules", "Readme", "Scripts", "Support", "Winuae", "Workbench" )
+$portableZipFile = Join-Path $releaseDir -ChildPath ("hstwb-installer.{0}-portable.zip" -f $hstwbInstallerVersion.ToLower())
 
-foreach($component in $components)
-{
-	$componentDir = Join-Path -Path $rootDir -ChildPath $component
+Write-Output "Build portable zip release"
+Write-Output "--------------------------"
+Write-Output "- Building portable zip release..."
 
-	if (!(Test-Path $componentDir))
-	{
-		continue
-	}
+# compress package directory
+[System.IO.Compression.ZipFile]::CreateFromDirectory($buildDir, $portableZipFile, 'Optimal', $false)
 
-	Copy-Item -Path $componentDir -Recurse -Destination $outputDir
-}
+Write-Output "Done."
+Write-Output ("Successfully build portable zip release file '{0}'." -f $portableZipFile)
+Write-Output ""
 
-Write-Host "Done." -ForegroundColor 'Green'
 
-# Harvest component directories to build wxs using wix toolset heat
-# -----------------------------------------------------------------
-
-Write-Host "Building wxs components from directories..." -ForegroundColor 'Yellow'
+# build msi release
+# -----------------
+Write-Output "Build msi release"
+Write-Output "-----------------"
+Write-Output "- Building wxs components from directories..."
 
 $components += "Packages"
 
 $wixToolsetHeatArgsComponents = @()
 
 # build heat args for each component
-$components | ForEach-Object { $wixToolsetHeatArgsComponents += ("dir ""{0}"" -o ""{0}.wxs"" -sreg -var var.{1}Dir -dr {1}ComponentDir -cg {1}ComponentGroup -sfrag -gg -g1" -f (Join-Path -Path $outputDir -ChildPath $_), $_.Replace('-', '')) }
+$components | ForEach-Object { $wixToolsetHeatArgsComponents += ("dir ""{0}"" -o ""{0}.wxs"" -sreg -var var.{1}Dir -dr {1}ComponentDir -cg {1}ComponentGroup -sfrag -gg -g1" -f (Join-Path -Path $buildDir -ChildPath $_), $_.Replace('-', '')) }
 
 # run heat with args for each component
-$wixToolsetHeatArgsComponents | ForEach-Object { StartProcess $wixToolsetHeatFile $_ $outputDir }
-
-Write-Host "Done." -ForegroundColor 'Green'
+$wixToolsetHeatArgsComponents | ForEach-Object { Start-Process $wixToolsetHeatFile -ArgumentList $_ -WorkingDirectory $buildDir -Wait -NoNewWindow -PassThru }
 
 
-# Copy hstwb installer wix files
-# ------------------------------
+# copy wix files
+Write-Output "- Copying wix files..."
 
-Write-Host "Copying HstWB Installer wix files..." -ForegroundColor 'Yellow'
+Copy-Item -Path (Resolve-Path '..\wix\*') -Recurse -Destination $buildDir
 
-Copy-Item -Path (Resolve-Path '..\wix\*') -Recurse -Destination $outputDir
-Copy-Item -Path (Resolve-Path '..\install.*') -Recurse -Destination $outputDir
-Copy-Item -Path (Resolve-Path '..\launcher.*') -Recurse -Destination $outputDir
-Copy-Item -Path (Resolve-Path '..\setup.*') -Recurse -Destination $outputDir
-Copy-Item -Path (Resolve-Path '..\run.*') -Recurse -Destination $outputDir
-Copy-Item -Path (Resolve-Path '..\LICENSE.txt') -Recurse -Destination $outputDir
-Copy-Item -Path (Resolve-Path '..\hstwb_installer.ico') -Recurse -Destination $outputDir
-
-# Update year in license files
-$licenseRtfFile = Join-Path $outputDir -ChildPath 'license.rtf'
+# update year in license rtf file
+$licenseRtfFile = Join-Path $buildDir -ChildPath 'license.rtf'
 $licenseRtfText = [System.IO.File]::ReadAllText($licenseRtfFile) -replace 'Copyright \(c\) \d+', ("Copyright (c) {0}" -f [System.DateTime]::Now.Year)
 [System.IO.File]::WriteAllText($licenseRtfFile, $licenseRtfText)
 
-$licenseTxtFile = Join-Path $outputDir -ChildPath 'LICENSE.txt'
-$licenseTxtText = [System.IO.File]::ReadAllText($licenseTxtFile) -replace 'Copyright \(c\) \d+', ("Copyright (c) {0}" -f [System.DateTime]::Now.Year)
-[System.IO.File]::WriteAllText($licenseTxtFile, $licenseTxtText)
 
-Write-Host "Done." -ForegroundColor 'Green'
-
-
-# Compile wxs using wix toolset candle
-# ------------------------------------
-
-Write-Host "Compiling wxs files..." -ForegroundColor 'Yellow'
+# compile wxs files
+Write-Output "- Compiling wxs files..."
+Write-Output ""
 
 $wixToolsetCandleArgs = ('-dVersion="' + ($hstwbInstallerVersion -replace '-[^\-]+$', '') + '" -dAmigaDir="Amiga" -dFontsDir="Fonts" -dFsUaeDir="Fs-Uae" -dImagesDir="Images" -dKickstartDir="Kickstart" -dLicensesDir="Licenses" -dModulesDir="Modules" -dPackagesDir="Packages" -dReadmeDir="Readme" -dScriptsDir="Scripts" -dSupportDir="Support" -dWinuaeDir="Winuae" -dWorkbenchDir="Workbench" "*.wxs"')
-#StartProcess $wixToolsetCandleFile $wixToolsetCandleArgs $outputDir
-$candleProcess = Start-Process $wixToolsetCandleFile -ArgumentList $wixToolsetCandleArgs -WorkingDirectory $outputDir -Wait -NoNewWindow -PassThru
+$candleProcess = Start-Process $wixToolsetCandleFile -ArgumentList $wixToolsetCandleArgs -WorkingDirectory $buildDir -Wait -NoNewWindow -PassThru
 
-if ($candleProcess.ExitCode -eq 0)
-{
-	Write-Host "Done." -ForegroundColor 'Green'
-}
-else
+if ($candleProcess.ExitCode -ne 0)
 {
 	Write-Host ("Error: WiX Candle failed with exit code {0}!" -f $candleProcess.ExitCode) -ForegroundColor 'Red'
 	exit 1
 }
 
 
+# link wixobj files
+$msiFile = Join-Path $releaseDir -ChildPath ("hstwb-installer.{0}.msi" -f $hstwbInstallerVersion.ToLower())
+Write-Output "- Linking wixobj files..."
+Write-Output ""
 
-# Link wixobj using wix toolset light
-# -----------------------------------
+$wixToolsetLightArgs = "-o ""{0}"" -ext WixUIExtension -ext WixUtilExtension ""*.wixobj""" -f $msiFile
+$lightProcess = Start-Process $wixToolsetLightFile -ArgumentList $wixToolsetLightArgs -WorkingDirectory $buildDir -Wait -NoNewWindow -PassThru
 
-Write-Host "Linking wixobj files..." -ForegroundColor 'Yellow'
-
-$wixToolsetLightArgs = "-o ""hstwb-installer.{0}.msi"" -ext WixUIExtension -ext WixUtilExtension ""*.wixobj""" -f ($hstwbInstallerVersion.ToLower())
-#StartProcess $wixToolsetLightFile $wixToolsetLightArgs $outputDir
-$lightProcess = Start-Process $wixToolsetLightFile -ArgumentList $wixToolsetLightArgs -WorkingDirectory $outputDir -Wait -NoNewWindow -PassThru
-
-if ($lightProcess.ExitCode -eq 0)
-{
-	Write-Host "Done." -ForegroundColor 'Green'
-}
-else
+if ($lightProcess.ExitCode -ne 0)
 {
 	Write-Host ("Error: WiX Light failed with exit code {0}!" -f $candleProcess.ExitCode) -ForegroundColor 'Red'
 	exit
 }
+
+Write-Output ""
+Write-Output "Done."
+Write-Output ("Successfully build msi release file '{0}'." -f $msiFile)
