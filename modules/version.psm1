53aabe9453df1de3dc57cb6c3a9da4a00e02ee1e
Henrik Stengaard <hst@firstrealize.com>
2018-05-20 14:49:47 +0200

    Updated version to 1.2.0-BETA5

--- modules/version.psm1
+++ modules/version.psm1
@@ -2,11 +2,11 @@
 # ------------------------------
 #
 # Author: Henrik Noerfjand Stengaard
-# Date:   2018-05-11
+# Date:   2018-05-20
 #
 # A powershell module for HstWB Installer with current version.
 
 function HstwbInstallerVersion()
 {
-    return '1.2.0-BETA4'
+    return '1.2.0-BETA5'
 }
