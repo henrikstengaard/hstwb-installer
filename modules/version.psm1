ba2299418877f7ae46ddd57e259199c47883fecd
Henrik Nørfjand Stengaard <henrik.steengaard@valtech.com>
2018-08-03 22:21:39 +0200

    Updated version

--- modules/version.psm1
+++ modules/version.psm1
@@ -2,11 +2,11 @@
 # ------------------------------
 #
 # Author: Henrik Noerfjand Stengaard
-# Date:   2018-05-20
+# Date:   2018-08-03
 #
 # A powershell module for HstWB Installer with current version.
 
 function HstwbInstallerVersion()
 {
-    return '1.2.0-BETA5'
+    return '1.2.0-BETA6'
 }
