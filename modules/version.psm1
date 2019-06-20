01bad649d8dc245aad0d707a1202878eeaaa67f9
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2019-06-20 11:04:08 +0200

    Bumped version to 1.3.0-BETA3

--- modules/version.psm1
+++ modules/version.psm1
@@ -2,11 +2,11 @@
 # ------------------------------
 #
 # Author: Henrik Noerfjand Stengaard
-# Date:   2019-03-11
+# Date:   2019-06-20
 #
 # A powershell module for HstWB Installer with current version.
 
 function HstwbInstallerVersion()
 {
-    return '1.3.0-BETA2'
+    return '1.3.0-BETA3'
 }
