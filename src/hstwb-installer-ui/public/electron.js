83d85e31b12a95088b3121161bbfd43d03d7ee35
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2019-08-15 13:38:06 +0200

    Added icons and logo

--- src/hstwb-installer-ui/public/electron.js
+++ src/hstwb-installer-ui/public/electron.js
@@ -14,7 +14,7 @@ let mainWindow;
 
 function createWindow() {
     // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow = new BrowserWindow({width: 800, height: 600, icon: __dirname + '/hstwb-installer.ico'});
 
     const startUrl = process.env.ELECTRON_START_URL || url.format({
         pathname: path.join(__dirname, '/../build/index.html'),
