e8855fb7c359078d05d81469f084975c83f9ae7a
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2019-08-18 22:55:09 +0200

    Added working electron open dialog

--- src/hstwb-installer-ui/src/components/Image.tsx
+++ src/hstwb-installer-ui/src/components/Image.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
- 
+import { Button } from '@material-ui/core';
+// @ts-ignore
+const { dialog } = window.require('electron').remote;
+
+// const { dialog } = require('electron')
+
 class Image extends React.Component {
+  openDialog = () => {
+    const selectedPaths = dialog.showOpenDialog();
+    console.log(selectedPaths);
+  }
+
   render() {
     return (
-      <h2>Image</h2>
+      <React.Fragment>
+        <h2>Image</h2>
+        <Button onClick={() => this.openDialog()}>Open</Button>
+      </React.Fragment>
     );
   }
 }
