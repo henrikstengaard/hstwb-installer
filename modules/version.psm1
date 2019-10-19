f109ab4e11d785b0299f9a28152c2aa0fcf954c7
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2019-10-19 23:00:13 +0200

    Bumped version to 1.4.2

--- modules/version.psm1
+++ modules/version.psm1
@@ -2,11 +2,11 @@
 # ------------------------------
 #
 # Author: Henrik Noerfjand Stengaard
-# Date:   2019-08-22
+# Date:   2019-10-19
 #
 # A powershell module for HstWB Installer with current version.
 
 function HstwbInstallerVersion()
 {
-    return '1.4.1'
+    return '1.4.2'
 }
