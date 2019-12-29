8b31c46c5f908c6d813e5481c44c2609dcc2fc9a
Henrik Noerfjand Stengaard <hst@firstrealize.com>
2019-12-29 09:20:02 +0000

    Add root partition size

--- launcher/amibian/setup/amibian/expand-filesystem.sh
+++ launcher/amibian/setup/amibian/expand-filesystem.sh
@@ -3,15 +3,17 @@
 # Expand Filesystem
 # -----------------
 # Author: Henrik Noerfjand Stengaard
-# Date: 2019-12-20
+# Date: 2019-12-29
 #
 # A bash script for Amibian to expand filesystem.
 
+# get root partition size
+SIZE="$(df -h --output=size ~ | tail -n 1 | awk '{$1=$1};1')"
 
 # show expand filesystem dialog
 dialog --clear --stdout \
 --title "Expand Filesystem" \
---yesno "Do you want to expand filesystem?" 0 0
+--yesno "Root partition size is \"$SIZE\" and it should be as large as possible. Do you want to expand filesystem?" 0 0
 
 # exit, if no is selected
 if [ $? -ne 0 ]; then
