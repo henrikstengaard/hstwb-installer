41168106d30fd927500bc3cc45511863b98f838c
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2020-04-04 16:13:58 +0100

    Add uninstall script

---
+++ launcher/amibian/uninstall.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -e
+
+# HstWB Installer Uninstall
+# -------------------------
+# Author: Henrik Noerfjand Stengaard
+# Date: 2020-04-04
+#
+# A bash script to uninstall HstWB Installer launcher for Amibian.
+
+# show uninstall dialog
+dialog --clear --stdout \
+--title "Uninstall HstWB Installer" \
+--yesno "This will remove HstWB Installer configuration and patches installed by HstWB Installer. Main HstWB Installer application and scripts will not be removed and HstWB Installer can be installed again be running './install.sh' script.\n\nDo you want to uninstall HstWB Installer?" 0 0
+
+# exit, if no is selected
+if [ $? -ne 0 ]; then
+  exit
+fi
+
+# delete hstwb installer profile, if it exist
+if [ -d ~/.hstwb-installer ]; then
+        rm -Rf ~/.hstwb-installer
+fi
+
+# delete hstwb from bin
+if [ -f "/usr/local/bin/hstwb" ]; then
+	rm "/usr/local/bin/hstwb"
+fi
+
+# restore backup of profile, if it exist
+if [ -f ~/.profile_backup -a -f ~/.profile ]; then
+	cp ~/.profile_backup ~/.profile
+fi
+
+# remove hstwb from amibian 1.4.1001 menu
+if [ -f /usr/local/bin/menu -a "$(grep -i "cat ~/.hstwb-installer/menu_files/hstwb" /usr/local/bin/menu)" != "" ]; then
+	grep -iv "cat ~/.hstwb-installer/menu_files/hstwb" /usr/local/bin/menu >/tmp/_menu
+	mv /tmp/_menu /usr/local/bin/menu
+	chmod +x /usr/local/bin/menu
+fi
+
+# show success dialog
+dialog --clear --title "Success" --msgbox "Successfully uninstalled HstWB Installer." 0 0
+
+
