965a83aeef42f71bf79a9afa0e9efe71a66ae311
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2020-04-04 08:24:58 +0100

    List fat32 devices in dialog

--- launcher/amibian/system/list-fat32-devices.sh
+++ launcher/amibian/system/list-fat32-devices.sh
@@ -3,11 +3,9 @@
 # List FAT32 devices
 # ------------------
 # Author: Henrik Noerfjand Stengaard
-# Date: 2020-03-04
+# Date: 2020-04-04
 #
 # A bash script to list FAT32 devices.
 
-# press enter to continue
-echo "List of FAT32 devices and their mountpoints"
-lsblk --list --paths --output name,fstype,size,state,mountpoint | grep -e "vfat"
-read -p "Press enter to continue"
+message=$(lsblk --list --paths --output name,fstype,size,state,mountpoint | grep -e "vfat")
+dialog --clear --title "List of FAT32 devices and their mountpoints" --msgbox "$message" 0 0
