24172486b78e9b3e7f9d5a85f8a0537d7b7edf81
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2020-04-04 08:25:33 +0100

    Add quiet option to mount fat32 devices

--- launcher/amibian/system/mount-fat32-devices.sh
+++ launcher/amibian/system/mount-fat32-devices.sh
@@ -3,36 +3,60 @@
 # Mount FAT32 devices
 # -------------------
 # Author: Henrik Noerfjand Stengaard
-# Date: 2020-03-04
+# Date: 2020-04-04
 #
 # A bash script to mount FAT32 devices to '/media/' using pmount.
 
-# show mount fat32 devices dialog
-dialog --clear --stdout \
---title "Mount FAT32 devices" \
---yesno "Mount FAT32 devices will go through devices with FAT32 filesystem and mount them to '/media/' using pmount, if they aren't already mounted. Use this if FAT32 formatted USB devices doesn't automatically appear in '/media/'.\n\nDo you want to mount FAT32 devices?" 0 0
+quiet=0
 
-# exit, if no is selected
-if [ $? -ne 0 ]; then
-	exit
+# parse arguments
+for i in "$@"
+do
+case $i in
+    -q|--quiet)
+    quiet=1
+    shift
+    ;;
+    *)
+        # unknown argument
+    ;;
+esac
+done
+
+# show dialog, if not quiet
+if [ $quiet -eq 0 ]; then
+	# show mount fat32 devices dialog
+	dialog --clear --stdout \
+	--title "Mount FAT32 devices" \
+	--yesno "Mount FAT32 devices will go through devices with FAT32 filesystem and mount them to '/media/' using pmount, if they aren't already mounted. Use this if FAT32 formatted USB devices doesn't automatically appear in '/media/'.\n\nDo you want to mount FAT32 devices?" 0 0
+
+	# exit, if no is selected
+	if [ $? -ne 0 ]; then
+		exit
+	fi
 fi
 
 # iterate through devices with fat32 filesystem
+clear
+mounted_devices=()
 while read devicepath; do
-	echo "Found FAT32 device '$devicepath'..."
-
 	# get mountpoint and skip, if not empty
 	mountpoint=$(lsblk --list --paths --noheadings --output mountpoint $devicepath)
 	if [ "$mountpoint" != "" ]; then
-	        echo "Skip, already mounted as '$mountpoint'..."
 		continue
 	fi
 
 	# mount fat32 device using pmount
-	echo "Mounting FAT32 device '$devicepath'..."
+	mounted_devices+=($devicepath)
 	sudo pmount --sync --umask 000 $devicepath
 done < <(lsblk --list --paths --noheadings --output name,fstype,size,state,mountpoint | grep -e "vfat" | awk '{print $1}')
 
-# list fat32 devices
-echo ""
-./list-fat32-devices.sh
+# show dialog with mounted devices, if any
+if [ ${#mounted_devices[@]} -ne 0 ]; then
+	dialog --clear --title "Mount FAT32 devices" --msgbox "Successfully mounted FAT32 devices:\n'${mounted_devices[@]}'" 0 0
+fi
+
+# list fat32 devices, if not quiet
+if [ $quiet -eq 0 ]; then
+	./list-fat32-devices.sh
+fi
