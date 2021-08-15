32008ae661f602ae0b6c6183c022f9676692acb0
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2021-08-15 21:46:14 +0200

    Fix update dh1 and dh2 harddisk icons. Fix execute unknown command using Amiga OS 3.1.4

--- modules/version.psm1
+++ modules/version.psm1
@@ -2,11 +2,11 @@
 # ------------------------------
 #
 # Author: Henrik Noerfjand Stengaard
-# Date:   2021-07-27
+# Date:   2021-08-15
 #
 # A powershell module for HstWB Installer with current version.
 
 function HstwbInstallerVersion()
 {
-    return '1.5.0'
+    return '1.5.1'
 }
