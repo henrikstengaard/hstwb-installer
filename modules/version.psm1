ea87bd209eb8528e957eb6947c834277c05c9119
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2022-01-16 10:26:54 +0100

    Bump version to 1.6.3

--- modules/version.psm1
+++ modules/version.psm1
@@ -2,11 +2,11 @@
 # ------------------------------
 #
 # Author: Henrik Noerfjand Stengaard
-# Date:   2022-01-04
+# Date:   2022-01-16
 #
 # A powershell module for HstWB Installer with current version.
 
 function HstwbInstallerVersion()
 {
-    return '1.6.2'
+    return '1.6.3'
 }
