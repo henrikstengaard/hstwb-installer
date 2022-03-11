11571feb7517d053e52dbe3dfce7e6d67ff3ba56
Henrik Nørfjand Stengaard <hst@firstrealize.com>
2022-03-12 00:13:34 +0100

    Fix run process extension

--- src/HstWbInstaller.Core/Extensions/ProcessExtensions.cs
+++ src/HstWbInstaller.Core/Extensions/ProcessExtensions.cs
@@ -3,18 +3,19 @@
     using System;
     using System.Diagnostics;
     using System.IO;
+    using System.Threading;
     using System.Threading.Tasks;
 
     public static class ProcessExtensions
     {
-        public static string RunProcess(this string command, string args = null, int successExitCode = 0)
+        public static string RunProcess(this string command, string args = null, string workingDirectory = null, int successExitCode = 0)
         {
-            var process = StartProcess(command, args);
-            
+            var process = StartProcess(command, args, workingDirectory);
+
             var standardOutput = process.StandardOutput.ReadToEnd(); 
             var standardError = process.StandardError.ReadToEnd();
 
-            if (process.ExitCode != successExitCode)
+            if (process.HasExited && process.ExitCode != successExitCode)
             {
                 throw new IOException(string.Concat(standardOutput, Environment.NewLine, standardError));
             }
@@ -22,14 +23,14 @@
             return standardOutput;
         }
 
-        public static async Task<string> RunProcessAsync(this string command, string args = null, int successExitCode = 0)
+        public static async Task<string> RunProcessAsync(this string command, string args = null, string workingDirectory = null, int successExitCode = 0, CancellationToken token = default)
         {
-            var process = StartProcess(command, args);
-            
+            var process = StartProcess(command, args, workingDirectory);
+
             var standardOutput = await process.StandardOutput.ReadToEndAsync(); 
             var standardError = await process.StandardError.ReadToEndAsync();
             
-            if (process.ExitCode != successExitCode)
+            if (process.HasExited && process.ExitCode != successExitCode)
             {
                 throw new IOException(string.Concat(standardOutput, Environment.NewLine, standardError));
             }
@@ -37,7 +38,7 @@
             return standardOutput;
         }
         
-        private static Process StartProcess(string command, string args = null)
+        private static Process StartProcess(string command, string args = null, string workingDirectory = null)
         {
             var process = Process.Start(
                 new ProcessStartInfo(command)
@@ -46,7 +47,8 @@
                     RedirectStandardError = true,
                     CreateNoWindow = true,
                     UseShellExecute = false,
-                    Arguments = args ?? string.Empty
+                    Arguments = args ?? string.Empty,
+                    WorkingDirectory = workingDirectory ?? string.Empty,
                 });
         
             if (process == null)
@@ -54,6 +56,8 @@
                 throw new IOException($"Failed to start process command '{command}' {(string.IsNullOrWhiteSpace(args) ? string.Empty : $" with arguments '{args}'")}");
             }
 
+            process.EnableRaisingEvents = true;
+            
             return process;
         }
     }
